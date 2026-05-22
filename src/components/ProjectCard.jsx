d81ba8f799eafbd2a5390051fb5d2542ba6ef92e
Lauzari <mlauramoyano1985@gmail.com>
2026-05-22 09:03:11 -0300

    fix images top

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,7 +6,7 @@ export default function ProjectCard({ project }) {
         <img
           src={project.image}
           alt={project.title}
-          className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500"
+          className="w-full h-full object-cover object-top  group-hover:scale-110 transition-transform duration-500"
         />
       </div>
 
